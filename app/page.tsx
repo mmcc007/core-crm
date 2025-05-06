df5387eec4b25674276780b3c85c423b16aa34c4
Maurice McCabe <mmcc007@gmail.com>
2025-05-06 11:53:35 -0700

    fix: add required value prop to RadioGroupItem component

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { RadioGroup, RadioGroupItem } from "../src/components/ui/radio-group"
+
+export default function SyntheticV0PageForDeployment() {
+  return (
+    <div>
+      <RadioGroup>
+        <RadioGroupItem value="option1" />
+      </RadioGroup>
+    </div>
+  )
+}
